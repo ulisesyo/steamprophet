c0b473799ed579ec9068a137b93c49d8c7e7f8cb
Ulises Martínez Piña <ulisesyo@gmail.com>
2017-08-08 17:37:58 +0200

    Min price is taken from Steamdb.com and now we can calculate the estimation

--- GetSalesNumbers.py
+++ GetSalesNumbers.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import re
 from bs4 import BeautifulSoup
 
 idx = 0
@@ -11,7 +12,7 @@ with open('games.txt', 'r') as f:
 f.closed
 
 with open('sales.tsv', 'w', encoding='utf-8') as f:
-    f.write("appid\tprice\tplayers\tvariance players\tmin players\n")
+    f.write("appid\tprice\tplayers\tplayers_variance\tmin players\tmin price\tearning estimation\n")
 f.closed
 
 with open('sales.tsv', 'a', encoding='utf-8') as f:
@@ -24,19 +25,28 @@ with open('sales.tsv', 'a', encoding='utf-8') as f:
         if (data['appid'] == 999999):
             f.write(str(g)+"\tHidden by developer")
         else:
-            # get lower price from steamdb
-            steamdb = requests.get('https://steamdb.info/app/' + str(g))
-            soup = BeautifulSoup(steamdb.content, 'html.parser')
-            dollarPrice = soup.select(".owned")
-            print("Lower price: " + str(dollarPrice))
-            
             minPlayers = data['players_forever'] - data['players_forever_variance']
+            minPlayers = 0 if minPlayers < 0 else minPlayers
             price = 0 if data['price'] == 0 else float(data['price'])/100.0
-            f.write(str(g) + "\t" + str(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\n")
-            
+
+            # get lower price from steamdb
+            try:
+                steamdb = requests.get('https://steamdb.info/app/' + str(g))
+                soup = BeautifulSoup(steamdb.content, 'html.parser')
+                dollarPrice = soup.select(".owned")[1].select("td")[3].text
+                priceAndDiscount = re.findall(r'[+-]?[0-9.]+', str(dollarPrice))
+            except:
+                f.write(str(g) + "\t" + str(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\tError at steamdb\n")
+                continue
+
+            if price > 0:
+                estimation = float(priceAndDiscount[0])*int(minPlayers)
+                print("Lower price: " + str(priceAndDiscount[0]) + "->" + str(estimation))
+                f.write(str(g) + "\t" + str(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\t" + str(priceAndDiscount[0]) + "\t" + str(estimation) + "\n")
+            else:
+                f.write(str(g) + "\tF2P\n")
         idx = idx + 1
         time.sleep(0.25)
-        break
             
 f.closed
 
