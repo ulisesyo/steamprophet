56c6c84ba2d49a4b4616af407701669cf4f5b866
Ulises Martínez Piña <ulises@nuberugames.com>
2017-08-14 11:40:14 +0200

    Improved error handling + improved estimation and prices display

--- GetSalesNumbers.py
+++ GetSalesNumbers.py
@@ -19,7 +19,7 @@ with open('sales.tsv', 'a', encoding='utf-8') as f:
     for g in gamesId:
         page = requests.get('http://steamspy.com/api.php?request=appdetails&appid=' + str(g))
         data = json.loads(page.text)
-        print("Parsing game: (" + str(data['appid']) + ") " + data['name'])
+        print("Parsing game: (" + str(data['appid']) + ") " + str(data['name']))
         # print(page.text)
         
         if (data['appid'] == 999999):
@@ -27,7 +27,7 @@ with open('sales.tsv', 'a', encoding='utf-8') as f:
         else:
             minPlayers = data['players_forever'] - data['players_forever_variance']
             minPlayers = 0 if minPlayers < 0 else minPlayers
-            price = 0 if data['price'] == 0 else float(data['price'])/100.0
+            price = 0 if data['price'] == None or data['price'] == 0 else float(data['price'])/100.0
 
             # get lower price from steamdb
             try:
@@ -36,13 +36,13 @@ with open('sales.tsv', 'a', encoding='utf-8') as f:
                 dollarPrice = soup.select(".owned")[1].select("td")[3].text
                 priceAndDiscount = re.findall(r'[+-]?[0-9.]+', str(dollarPrice))
             except:
-                f.write(str(g) + "\t" + str(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\tError at steamdb\n")
+                f.write(str(g) + "\t" + "{0:.2f}".format(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\t0\t0\tError at steamdb\n")
                 continue
 
             if price > 0:
                 estimation = float(priceAndDiscount[0])*int(minPlayers)
-                print("Lower price: " + str(priceAndDiscount[0]) + "->" + str(estimation))
-                f.write(str(g) + "\t" + str(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\t" + str(priceAndDiscount[0]) + "\t" + str(estimation) + "\n")
+                print("Lower price: " + str(priceAndDiscount[0]) + "-> Estimation: " + "{0:.2f}".format(estimation))
+                f.write(str(g) + "\t" + "{0:.2f}".format(price) + "\t" + str(data['players_forever']) + "\t" + str(data['players_forever_variance']) + "\t" + str(minPlayers) + "\t" + str(priceAndDiscount[0]) + "\t" + "{0:.2f}".format(estimation) + "\n")
             else:
                 f.write(str(g) + "\tF2P\n")
         idx = idx + 1
